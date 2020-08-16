9925c4cabf95d3ed42af48c900a13608b4e45eae
yuto14 <programming1410@gmail.com>
2020-08-16 11:12:29 +0900

    [finish]Lesson5

---
+++ Greeting.java
@@ -0,0 +1,14 @@
+// クラス
+class Greeting {
+    // メソッド
+    public static void main(String[] args) {
+
+        System.out.println("Good afternoon");
+         System.out.println("Good morning");
+        System.out.println("Good evening");
+    }
+}
+
+// class Greeting {
+// }
+// までがブロック
